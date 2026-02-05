908e5d678153e69197fd464dd94bb64fe23525b8
ricardo.andrade <ricardo.andrade05@gmail.com>
2026-02-04 22:25:45 -0300

    Implementação Genre

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,11 +1,18 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Genre;
+
 public class GenreDTO {
 
     private Long id;
     private String name;
+    
+    public GenreDTO(Genre genre) {
+		this.id = genre.getId();
+		this.name = genre.getName();
+    }
 
-    public Long getId() {
+	public Long getId() {
         return id;
     }
 
--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,8 +1,24 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 
 @Service
 public class GenreService {
+
+	@Autowired
+	private GenreRepository repository;
+	
+	public List<GenreDTO> findAll() {
+		List<Genre> list = repository.findAll(Sort.by("id"));
+		return list.stream().map(x -> new GenreDTO(x)).toList();
+	}
 
 }
